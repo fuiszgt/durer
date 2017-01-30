0215401cdb4c5aa5bc8e6a0502be192fabed5ce7
Zsofi <molnar.saska.zsofi@gmail.com>
2017-01-30 16:24:09 +0100

    add schedul and remove personalization

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,18 @@ import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
+import { SchedulePage } from '../pages/schedule/schedule';
+
+import { ConferenceData } from '../providers/conference-data';
 
 @NgModule({
   declarations: [
     MyApp,
     AboutPage,
     ContactPage,
-    HomePage,
-    TabsPage
+    TabsPage,
+    SchedulePage
   ],
   imports: [
     IonicModule.forRoot(MyApp)
@@ -22,9 +24,9 @@ import { TabsPage } from '../pages/tabs/tabs';
     MyApp,
     AboutPage,
     ContactPage,
-    HomePage,
-    TabsPage
+    TabsPage,
+    SchedulePage
   ],
-  providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
+  providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}, ConferenceData]
 })
 export class AppModule {}
---
+++ src/pages/schedule/schedule.ts
@@ -0,0 +1,88 @@
+import { Component, ViewChild } from '@angular/core';
+
+import { AlertController, App, FabContainer, ItemSliding, List, ModalController, NavController, LoadingController } from 'ionic-angular';
+
+/*
+  To learn how to use third party libs in an
+  Ionic app check out our docs here: http://ionicframework.com/docs/v2/resources/third-party-libs/
+*/
+// import moment from 'moment';
+
+import { ConferenceData } from '../../providers/conference-data';
+/*import { ScheduleFilterPage } from '../schedule-filter/schedule-filter';*/
+import { SessionDetailPage } from '../session-detail/session-detail';
+
+@Component({
+  selector: 'page-schedule',
+  templateUrl: 'schedule.html'
+})
+export class SchedulePage {
+  // the list is a child of the schedule page
+  // @ViewChild('scheduleList') gets a reference to the list
+  // with the variable #scheduleList, `read: List` tells it to return
+  // the List and not a reference to the element
+  @ViewChild('scheduleList', { read: List }) scheduleList: List;
+
+  dayIndex = 0;
+  queryText = '';
+  segment = 'all';
+  excludeTracks: any = [];
+  shownSessions: any = [];
+  groups: any = [];
+  confDate: string;
+
+  constructor(
+    public alertCtrl: AlertController,
+    public app: App,
+    public loadingCtrl: LoadingController,
+    public modalCtrl: ModalController,
+    public navCtrl: NavController,
+    public confData: ConferenceData,
+  ) {}
+
+  ionViewDidLoad() {
+    this.app.setTitle('Schedule');
+    this.updateSchedule();
+  }
+
+  updateSchedule() {
+    // Close any open sliding items when the schedule updates
+    this.scheduleList && this.scheduleList.closeSlidingItems();
+
+    this.confData.getTimeline(this.dayIndex, this.queryText, this.excludeTracks, this.segment).subscribe((data: any) => {
+      this.shownSessions = data.shownSessions;
+      this.groups = data.groups;
+    });
+  }
+
+  /*presentFilter() {
+    let modal = this.modalCtrl.create(ScheduleFilterPage, this.excludeTracks);
+    modal.present();
+
+    modal.onWillDismiss((data: any[]) => {
+      if (data) {
+        this.excludeTracks = data;
+        this.updateSchedule();
+      }
+    });
+
+  }*/
+
+  goToSessionDetail(sessionData: any) {
+    // go to the session detail page
+    // and pass in the session data
+    this.navCtrl.push(SessionDetailPage, sessionData);
+  }
+
+
+  openSocial(network: string, fab: FabContainer) {
+    let loading = this.loadingCtrl.create({
+      content: `Posting to ${network}`,
+      duration: (Math.random() * 1000) + 500
+    });
+    loading.onWillDismiss(() => {
+      fab.close();
+    });
+    loading.present();
+  }
+}
